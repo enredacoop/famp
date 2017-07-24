6020d29e1ffe4203a61c9764bf6feb81a0c8c0b8
pr3ssh <pablo.martin@enreda.coop>
2017-07-24 17:39:08 +0200

    Change basemaps from dark to light

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ var myIcon = L.Icon.extend({
 
 var map = L.map('map').setView([37.344,-4.548], 8); // Andalucía
 
-var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
