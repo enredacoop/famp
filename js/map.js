604778f59a1d742ee0bd76faa6bbec958a76b9ed
pr3ssh <pablo.martin@enreda.coop>
2017-07-24 17:44:38 +0200

    Disable scroll wheel zoom on map

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ var myIcon = L.Icon.extend({
 });
 
 
-var map = L.map('map').setView([37.344,-4.548], 8); // Andalucía
+var map = L.map('map', {scrollWheelZoom: false}).setView([37.344,-4.548], 8); // Andalucía
 
 var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
