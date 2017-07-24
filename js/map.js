ab5f789b8055abe43be4d3fca51aedaa581a7f05
pr3ssh <pablo.martin@enreda.coop>
2017-07-24 16:56:17 +0200

    Colorize the map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,18 @@
+var myIcon = L.Icon.extend({
+    options: {
+        iconUrl: 'famp-icon.png',
+        iconSize:     [32, 32]
+    }
+});
+
+
 var map = L.map('map').setView([37.344,-4.548], 8); // Andalucía
 
-var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
+
+
 
 function popup(feature, layer) { 
     if (feature.properties.nombre) 
@@ -11,12 +21,16 @@ function popup(feature, layer) {
     } 
 }
 
+function styledPoints (feature, latlng) {
+    return L.marker(latlng, {icon: new myIcon()});
+}
+
 var ciudades = L.geoJson(null, {
-    onEachFeature: popup
+    onEachFeature: popup,
+    pointToLayer: styledPoints
 });
 
 $.getJSON("./js/ciudades.geojson", function (data) {
-    console.log(data);
     ciudades.addData(data);
 });
 
